f570e7d752220d001798d9b036f3349a9c8540cf
Raphael <raphaelrpais@gmail.com>
2018-03-23 18:42:37 -0300

    commit inicial de verdade

---
+++ scripts.js
@@ -0,0 +1,7 @@
+$(function() {
+    //ripple
+    var btns = document.querySelectorAll('.mdc-button');
+    for (var i = 0, btn; btn = btns[i]; i++) {
+        mdc.ripple.MDCRipple.attachTo(btn);
+    }
+});
