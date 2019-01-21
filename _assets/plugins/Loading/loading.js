edcaef9bc4f33b1bc8845e459854b1252b437208
Raphael <raphaelrpais@gmail.com>
2019-01-21 17:05:01 -0200

    unbelievable loading

--- _assets/plugins/Loading/loading.js
+++ _assets/plugins/Loading/loading.js
@@ -1,10 +1,20 @@
-document.onreadystatechange = function () {
-  var state = document.readyState
-  if (state == 'interactive') {
-       document.getElementById('content').style.visibility="hidden";
-  } else if (state == 'complete') {
-         document.getElementById('interactive');
-         document.getElementById('content').style.visibility="visible";
-         document.getElementById('load').style.visibility="hidden";
-  }
-}
+//document.onreadystatechange = function () {
+//  var state = document.readyState
+//  if (state == 'interactive') {
+//       document.getElementById('content').style.visibility="hidden";
+//  } else if (state == 'complete') {
+//         document.getElementById('interactive');
+//         document.getElementById('content').style.visibility="visible";
+//         document.getElementById('load').style.visibility="hidden";
+//  }
+//}
+
+window.addEventListener("load", function(event) {
+    document.getElementById('content').style.visibility="visible";
+    document.getElementById('load').style.visibility="hidden";
+});
+
+//window.onload = function () {
+//    document.getElementById('content').style.visibility="visible";
+//    document.getElementById('load').style.visibility="hidden";
+//}
