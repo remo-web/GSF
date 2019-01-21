8bf4036f501bc79dfee4917361f906405f21a40f
Raphael <raphaelrpais@gmail.com>
2019-01-21 18:24:25 -0200

    loading geral!
    Será?

--- _assets/plugins/Loading/loading.js
+++ _assets/plugins/Loading/loading.js
@@ -1,19 +1,3 @@
-//document.onreadystatechange = function () {
-//  var state = document.readyState
-//  if (state == 'interactive') {
-//       document.getElementById('content').style.visibility="hidden";
-//  } else if (state == 'complete') {
-//         document.getElementById('interactive');
-//         document.getElementById('content').style.visibility="visible";
-//         document.getElementById('load').style.visibility="hidden";
-//  }
-//}
-
-//window.addEventListener("load", function(event) {
-//    document.getElementById('content').style.visibility="visible";
-//    document.getElementById('load').style.visibility="hidden";
-//});
-
 window.onload = function () {
     document.getElementById('content').style.visibility="visible";
     document.getElementById('load').style.visibility="hidden";
