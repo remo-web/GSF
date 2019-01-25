ece760b943b8a9f6ebad84e5e08834b88aefa38c
Raphael <raphaelrpais@gmail.com>
2019-01-25 16:53:06 -0200

    snackbar

--- _assets/js/home.js
+++ _assets/js/home.js
@@ -3,4 +3,12 @@ $(function() {
         $(this).children( ".gsf-home--bg" ).toggleClass( "gsf-home--bg__mouseover" );
         $(this).children( ".gsf-home--text" ).toggleClass( "gsf-home--text__mouseover" );
     });
+    
+    const snackbar = document.querySelector('.mdc-snackbar');
+    
+    setTimeout(
+        function() {
+            $('.mdc-snackbar__surface').css("opacity", "1");
+            $('.mdc-snackbar__surface').css("transform", "scale(1)");
+        }, 3000);
 });
